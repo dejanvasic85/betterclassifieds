a810b6d52994565894c18a1d2152608a6982e983
Dejan Vasic <dejan.vasic@paramountit.com.au>
2013-07-11 17:11:58 +1000

    Adjusting the way connection string is read for App User Connection

---
+++ WebApplication/BetterClassified.UI/Framework/Security/ClassifiedSqlMembershipProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web.Security;
+using Paramount.ApplicationBlock.Configuration;
+using Paramount.ApplicationBlock.Data;
+
+
+namespace BetterClassified.UI.Framework.Security
+{
+    public class ClassifiedSqlMembershipProvider : SqlMembershipProvider
+    {
+        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+        {
+            base.Initialize(name, config);
+
+            var connectionString = ConfigReader.GetConnectionString("paramount/services", "AppUserConnection");
+
+            // Set private property of Membership provider.
+            var baseType = GetType().BaseType;
+            if (baseType != null)
+            {
+                FieldInfo connectionStringField = baseType.GetField("_sqlConnectionString", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (connectionStringField != null) connectionStringField.SetValue(this, connectionString);
+            }
+        }
+    }
+}

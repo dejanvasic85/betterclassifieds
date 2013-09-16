7e26b8ce471bf4e6cba9b40cdd68f6da5a3c3a5c
Dejan Vasic <dejan.vasic@paramountit.com.au>
2013-09-16 10:09:20 +1000

    Rafactor: removing unused code

--- WebApplication/iFlog/Controls/ParamountCompositeControl.vb
+++
@@ -1,9 +0,0 @@
-Namespace Controls
-    Public MustInherit Class ParamountCompositeControl
-        Inherits CompositeControl
-
-        Public Sub Redirect(ByVal url As String)
-            HttpContext.Current.Response.Redirect(url)
-        End Sub
-    End Class
-End Namespace

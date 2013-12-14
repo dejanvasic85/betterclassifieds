f1db53851925ed71fd14387da8fbd4466ce47e06
Dejan Vasic <dejan.vasic@paramountit.com.au>
2013-12-15 00:50:09 +1100

    Centralising the version management and commenting out the module registration just for now.

---
+++ WebApplication/iFlog/PostDeploy.ps1
@@ -0,0 +1,7 @@
+﻿$configFiles = Get-Childitem web.config
+foreach ($file in $configFiles)
+{ 
+    $content = Get-Content $file.PSPath 
+    $content = $content -replace "{version}","2.7"
+    [IO.File]::WriteAllText($file.FullName, ($content -join "`r`n"))
+}

2fb6ed2db985bdad4336972c61399422ca84310b
Dejan Vasic <dejan.vasic@paramountit.com.au>
2014-01-20 22:52:36 +1100

    Implementing smoke testing on QA environment...

---
+++ Tests/Paramount.Betterclassifieds.Tests.Functional/Deploy.ps1
@@ -0,0 +1,52 @@
+﻿# Map incoming variables from octopus
+$deployPath = $OctopusPackageDirectoryPath
+
+# Hard code some variables
+$resultsFileName = "CI-Results.xml"
+$reportFileName = "CI-Report.html"
+
+# Concat tools executables
+$nunit = ($deployPath + "\NUnit\nunit-console.exe")
+$specflow = ($deployPath + "\Specflow\specflow.exe")
+
+if ( $RunIntegrationTests -eq $true ){
+	
+	Write-Host "Starting functional tests using selenium with nunit ..."
+	& $nunit "Paramount.Betterclassifieds.Tests.Functional" "/noshadow" "/framework:net-4.0" ("/xml:" + $resultsFileName) # "/include:ci"
+
+	Write-Host "Generating report..."
+	& $specflow "nunitexecutionreport" "Paramount.Betterclassifieds.Tests.Functional.csproj" ("/xmlTestResult:" + $resultsFileName) ("/out:" + $reportFileName)
+
+	$xd = New-Object System.Xml.XmlDocument
+    $xd.Load($deployPath + "\" + $resultsFileName)
+   
+    $node = $xd.selectSingleNode("test-results")
+    [int]$errorCount = $node.getAttribute("errors")
+	[int]$failureCount = $node.getAttribute("failures")
+
+    if ( ($errorCount -gt 0) -or ($failureCount -gt 0) ) {
+		Write-Host "Smoke test completed with at least 1 error or failure"
+		Write-Host "Errored: $($errorCount)"
+		Write-Host "Failed: $($failureCount)"
+
+    	$smtpFrom = "smoketest@paramountit.com.au"
+    	$smtpTo = "support@paramountit.com.au"
+    	$messageSubject = "Smoke Test Failed"
+    	
+    	$message = New-Object System.Net.Mail.MailMessage $smtpfrom, $smtpto
+    	$message.Subject = $messageSubject
+    	$message.IsBodyHTML = $true
+    	$message.Body = Get-Content ($deployPath + "\" + $reportFileName)
+
+    	$smtp = New-Object Net.Mail.SmtpClient -ArgumentList ("smtpcorp.com", 2525)
+        $smtp.Credentials = New-Object System.Net.NetworkCredential("support@paramountit.com.au", "rs-101");
+    	$smtp.Send($message)
+        
+	}
+
+	Write-Host "Functional Tests completed..."
+}
+else {
+
+	Write-Host "Functional Tests have not been configured to run..."
+}
--- Tests/Paramount.Betterclassifieds.Tests.Functional/Deploy_Disabled.ps1
+++
@@ -1,5 +0,0 @@
-﻿# - Todo - 
-# 1. Call the powershell script to drop and create the database
-# 2. Call the nunit runner
-# 3. Call the specflow reporting tool
-# 4. Email the the results

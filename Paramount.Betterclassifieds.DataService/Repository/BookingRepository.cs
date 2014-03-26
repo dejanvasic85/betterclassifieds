93827ace434722cc77f60c23d7eb68c6ffccdbce
Dejan Vasic <dejan.vasic@paramountit.com.au>
2014-03-26 14:40:12 +1100

    Fixing filter for to include the end date for latest ads.

--- Paramount.Betterclassifieds.DataService/Repository/BookingRepository.cs
+++ Paramount.Betterclassifieds.DataService/Repository/BookingRepository.cs
@@ -91,7 +91,7 @@ namespace Paramount.Betterclassifieds.DataService.Repository
             using (var context = DataContextFactory.CreateClassifiedContext())
             {
                 var data = context.AdBookings
-                    .Where(b => b.BookingStatus == (int)BookingStatusType.Booked)
+                    .Where(b => b.BookingStatus == (int)BookingStatusType.Booked && b.EndDate > DateTime.Today)
                     .OrderByDescending(b => b.BookingDate) // Latest first
                     .Take(takeAmount)
                     .AsEnumerable();

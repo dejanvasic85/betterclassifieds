0012df84ee89bd4785b5e97ef8ff7a496c29c1df
Dejan Vasic <dejan.vasic@paramountit.com.au>
2014-04-07 23:36:11 +1000

    Adding client side validation to the login form

--- Paramount.Betterclassifieds.Presentation/ViewModels/LoginViewModel.cs
+++ Paramount.Betterclassifieds.Presentation/ViewModels/LoginViewModel.cs
@@ -31,35 +31,42 @@ namespace Paramount.Betterclassifieds.Presentation.ViewModels
         [Required]
         [Display(Name = "Email")]
         [DataType(DataType.EmailAddress)]
+        [StringLength(50)]
         public string RegisterEmail { get; set; }
 
         [Required]
         [Display(Name = "Confirm Email")]
         [Compare("RegisterEmail")]
         [DataType(DataType.EmailAddress)]
+        [StringLength(50)]
         public string ConfirmEmail { get; set; }
 
         [Required]
         [Display(Name = "Password")]
         [DataType(DataType.Password)]
+        [StringLength(50)]
         public string RegisterPassword { get; set; }
 
         [Required]
         [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
+        [StringLength(50)]
         public string ConfirmPassword { get; set; }
 
         [Required]
         [Display(Name = "First Name")]
+        [StringLength(25)]
         public string FirstName { get; set; }
 
         [Required]
         [Display(Name = "Last Name")]
+        [StringLength(25)]
         public string LastName { get; set; }
 
         [Required]
         [Display(Name = "Post Code")]
         [DataType(DataType.PostalCode)]
+        [StringLength(6)]
         public string PostCode { get; set; }
     }
 }

1d85b402e4fd64f2acd697333f2fbaf526967079
Dejan Vasic <dejan.vasic@paramountit.com.au>
2014-05-10 12:40:22 +1000

    The search results will hide the button if there are no more results.

--- WebApplication/iFlog/Scripts/Listings/Find.js
+++ WebApplication/iFlog/Scripts/Listings/Find.js
@@ -5,7 +5,8 @@
 
 (function ($) {
 
-    var maxPageRequests = parseInt( $('#hdnMaxPageRequests').val() );
+    var maxPageRequests = parseInt($('#hdnMaxPageRequests').val());
+    var resultsPerPage = parseInt($('#hdnResultsPerPage').val());
     var currentPage = 1;
 
     // JQuery on-ready
@@ -16,7 +17,8 @@
 
             $.post(me.data().url, {page : currentPage}).done(function (adListHtml) {
                 var $items = $(adListHtml).find('.list-group-item');
-                if ($items.length == 0) {
+                debugger;
+                if ($items.length == 0 || $items.length < resultsPerPage) {
                     me.hide();
                 } else {
                     $('.list-group').append($(adListHtml).find('.list-group-item'));

66f22f2fb62dc76f7cd6f497795af268f3d8f527
Dejan Vasic <dejan.vasic@paramountit.com.au>
2014-07-27 22:58:49 +1000

    Merging and adding new class as concept

---
+++ Paramount.Betterclassifieds.NextGen/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paramount.Betterclassifieds.NextGen
+{
+    // Todo - Pricing
+
+    public interface IBookingRepository
+    {
+    }
+
+    public interface IBookingManager
+    {
+        List<Category> GetCategories();
+
+        int StartBooking(int categoryId);
+
+        List<Channel> GetPublicationsForCategory(int categoryId);
+
+        void SetChannels(List<int> channels);
+
+        void SetState(BookingState bookingState, int adBookingId);
+
+        BookingState GetState(int adBookingId);
+
+        PriceSummary CalculatePrice(int adBookingId);
+        
+        PaymentStatus GetPaymentStatus(int adBookingId);
+
+        void SetPaymentStatus(PaymentStatus paymentStatus, int adBookingId);
+
+        PaymentMethod GetPaymentMethod(int adBookingId);
+
+        void SetPaymentMethod(PaymentMethod paymentMethod, int adBookingId);
+
+        void SetAd(Ad adDetails, int adBookingId);
+
+        Ad GetAdPreview(int adBookingId);
+
+        string Submit(int adbookingId);
+    }
+
+    public class PriceSummary
+    {
+    }
+
+    public class Category { }
+
+    public class Channel { }
+
+    public class BookingState { }
+
+    public class PaymentStatus { }
+
+    public class PaymentMethod { }
+
+    public class Ad { }
+}

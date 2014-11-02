8774c588a0ca10036c5aa1db955f5ceec3f65389
Dejan Vasic <dejan.vasic@paramountit.com.au>
2014-11-02 22:07:14 +1100

    Adjusting the database scripts to suit new variables.

--- Database/MembershipDatabase/PostDeploy.ps1
+++ Database/MembershipDatabase/PostDeploy.ps1
@@ -3,6 +3,7 @@
 	$BackupDatabasePath = "D:\"
 	$RestoreDatabase = $true
 	$DropCreateDatabase = $true
+	$SqlFilesPath = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\"
 #>
 
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
@@ -16,7 +17,7 @@ $db = Invoke-SqlCmd -Query "SELECT name from master.dbo.sysdatabases WHERE name 
 
 # Backup-SqlDatabase 
 if ( $BackupDatabase -eq $true -and $db -ne $null ){
-	$backupFile = $BackupDatabaseLocation + $connection.InitialCatalog + ".bak"    
+	$backupFile = $BackupDatabasePath + $connection.InitialCatalog + ".bak"    
     Write-Host "Backing Up..."
     Backup-SqlDatabase -ServerInstance $connection.DataSource -Database $connection.InitialCatalog -BackupFile $backupFile -BackupAction Database -Initialize
     Set-Location $scriptPath
@@ -24,12 +25,12 @@ if ( $BackupDatabase -eq $true -and $db -ne $null ){
 
 # Restore-SqlDatabase
 if ( $RestoreDatabase -eq $true ){	
-    $backupFile = $BackupDatabasePath + $connection.InitialCatalog + ".bak"
+    $backupFile = $BackupDatabasePath + "iFlogAppUser.bak"
 	Invoke-Sqlcmd "ALTER DATABASE [$($connection.InitialCatalog)] set SINGLE_USER with rollback immediate;" -ServerInstance $connection.DataSource  -QueryTimeout 0  -Username $connection.UserID -Password $connection.Password
 	Invoke-Sqlcmd "ALTER DATABASE [$($connection.InitialCatalog)] set RESTRICTED_USER with rollback immediate;" -ServerInstance $connection.DataSource  -QueryTimeout 0  -Username $connection.UserID -Password $connection.Password
 
-	$mdfRelocate = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile -ArgumentList ("AppUserTest", "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\$($connection.InitialCatalog).mdf")
-    $logRelocate = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile -ArgumentList ("AppUserTest_log", "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\$($connection.InitialCatalog)_log.ldf")
+	$mdfRelocate = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile -ArgumentList ("AppUserTest", "$($SqlFilesPath)DATA\$($connection.InitialCatalog).mdf")
+    $logRelocate = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile -ArgumentList ("AppUserTest_log", "$($SqlFilesPath)DATA\$($connection.InitialCatalog)_log.ldf")
 
 	Write-Host "Restoring Database $($connection.InitialCatalog) from $($backupFile) ..."
 	

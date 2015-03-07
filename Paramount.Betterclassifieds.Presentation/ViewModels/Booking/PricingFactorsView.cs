547ad871402d5eaf97bc6bf6d823a645090359bd
Dejan Vasic <dejan.vasic@paramountit.com.au>
2015-03-07 22:05:21 +1100

    Conflict with last commit

---
+++ Paramount.Betterclassifieds.Presentation/ViewModels/Booking/PricingFactorsView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Paramount.Betterclassifieds.Presentation.ViewModels.Booking
+{
+    public class PricingFactorsView
+    {
+        public string LineAdText { get; set; }
+        public string LineAdHeader { get; set; }
+        public bool IsSuperBoldHeader { get; set; }
+
+        public int Editions { get; set; }
+    }
+}

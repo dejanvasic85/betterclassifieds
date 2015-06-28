00fcea11e1dec76875e8a935d3abd1f5d0fb7d47
Dejan Vasic <dejan.vasic@paramountit.com.au>
2015-06-28 18:30:48 +1000

    UI enhancements for events

--- Paramount.Betterclassifieds.Presentation/Scripts/app/events/eventAd.model.js
+++ Paramount.Betterclassifieds.Presentation/Scripts/app/events/eventAd.model.js
@@ -11,11 +11,26 @@
         me.title = ko.observable(data.Title);
         me.description = ko.observable(data.Description);
         me.location = ko.observable(data.Location);
-        me.locationGeometry = ko.observable();
+        me.locationLat = ko.observable(data.LocationLat);
+        me.locationLong = ko.observable(data.LocationLong);
         me.reachedMaxImgCount = ko.observable(false);
-        me.errorMsg = ko.observable('');
+        
         me.uploadImageInProgress = ko.observable(false);
         me.adImages = ko.observableArray();
+
+        me.startDate = ko.observable(data.StartDate);
+        me.startTimeHours = ko.observable();
+        me.startTimeMinutes = ko.observable();
+        me.endDate = ko.observable();
+        me.endTimeHours = ko.observable();
+        me.endTimeMinutes = ko.observable();
+        me.images = ko.observableArray();
+        me.imageCountLimit = ko.observable(options.maxImages);
+        me.organiserName = ko.observable();
+        me.organiserPhone = ko.observable();
+        me.adStartDate = ko.observable();
+
+        me.errorMsg = ko.observable('');
     };
 
 

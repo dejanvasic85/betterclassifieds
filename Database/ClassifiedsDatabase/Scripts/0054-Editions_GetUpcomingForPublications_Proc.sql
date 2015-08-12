9eb5c6e34871cd75338536ef4d589260e89c0fce
Dejan Vasic <dejan.vasic@paramountit.com.au>
2015-08-12 18:39:44 +1000

    Bug fix to fetch the editions properly... What a mistake that was

---
+++ Database/ClassifiedsDatabase/Scripts/0054-Editions_GetUpcomingForPublications_Proc.sql
@@ -0,0 +1,26 @@
+﻿
+
+ALTER PROCEDURE [dbo].[Editions_GetUpcomingForPublications]
+     @publications		varchar(20),
+	 @minEditionDate	datetime,
+	 @minDeadlineDate	datetime
+
+	 /*
+		exec [Editions_GetUpcomingForPublications] 
+			@publications = '1,2,3,4,5,6',
+			@minEditionDate = '09-MAR-2015',
+			@minDeadlineDate = '09-MAR-2015'
+	 */
+AS
+BEGIN
+	
+
+	SELECT	DISTINCT EditionDate
+	FROM	dbo.Edition e
+	JOIN	SplitStringToInt(@publications, ',') p
+		ON	p.Data = e.PublicationId
+	WHERE	e.EditionDate >= @minEditionDate
+	AND		e.Deadline > @minDeadlineDate
+	ORDER BY e.EditionDate
+
+END

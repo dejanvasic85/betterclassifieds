be5f028d97bd6cdd9aad9c0e517caeb88db3f7a6
Dejan Vasic <dejan.vasic@paramountit.com.au>
2015-08-14 17:08:49 +1000

    The event table was actually missing

--- Database/ClassifiedsDatabase/Scripts/0053-Event_Table_Create.sql
+++ Database/ClassifiedsDatabase/Scripts/0053-Event_Table_Create.sql
@@ -1,4 +1,30 @@
 ﻿
+GO
+
+/****** Object:  Table [dbo].[Event]    Script Date: 14/08/2015 5:08:07 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Event](
+	[EventId] [int] IDENTITY(1,1) NOT NULL,
+	[Location] [nvarchar](max) NULL,
+	[LocationLatitude] [decimal](18, 2) NULL,
+	[LocationLongitude] [decimal](18, 2) NULL,
+	[EventStartDate] [datetime] NULL,
+	[EventEndDate] [datetime] NULL,
+ CONSTRAINT [PK_dbo.Event] PRIMARY KEY CLUSTERED 
+(
+	[EventId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+
+
 /****** Object:  Table [dbo].[EventTicket]    Script Date: 28/07/2015 8:31:22 PM ******/
 SET ANSI_NULLS ON
 GO

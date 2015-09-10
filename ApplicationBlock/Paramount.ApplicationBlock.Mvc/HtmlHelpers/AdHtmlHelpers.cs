dc0c6953d9c4dcb697eb5cae2050d56fe2dc985d
Dejan Vasic <dejanvasic24@gmail.com>
2015-09-10 22:03:47 +1000

    Adding contact details for the event if has value

---
+++ ApplicationBlock/Paramount.ApplicationBlock.Mvc/HtmlHelpers/AdHtmlHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Paramount.ApplicationBlock.Mvc.HtmlHelpers
+{
+    public static class AdHtmlHelpers
+    {
+        public static MvcHtmlString AdContactDetail<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, string glyph)
+        {
+            var data = (string)ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData).Model;
+
+            if (data.IsNullOrEmpty())
+                return MvcHtmlString.Empty;
+
+            var builder = new StringBuilder();
+            builder.Append("<tr>");
+            builder.AppendFormat("<td><span class='glyphicon glyphicon-{0}'><span></td>", glyph);
+            builder.AppendFormat("<td><span id='contactName'>{0}</span></td>", data);
+            builder.Append("</tr>");
+
+            return new MvcHtmlString(builder.ToString());
+        }
+    }
+}

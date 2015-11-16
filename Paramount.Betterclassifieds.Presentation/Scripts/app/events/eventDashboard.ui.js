a2df4a56fd290dcb13fce0561c3d1616a75509de
Dejan Vasic <dejanvasic24@gmail.com>
2015-11-16 22:18:50 +1100

    Creating the stub html and css for the event dashboard cards

--- Paramount.Betterclassifieds.Presentation/Scripts/app/events/eventDashboard.ui.js
+++ Paramount.Betterclassifieds.Presentation/Scripts/app/events/eventDashboard.ui.js
@@ -8,6 +8,28 @@
             var rootElement = $('.event-dashboard');
             var eventDashoardModel = new $paramount.models.EventDashboardModel(eventDashboardViewModel);
             ko.applyBindings(eventDashoardModel, rootElement.get(0));
+            
+            var ctx = document.getElementById("ticketSalesChart").getContext("2d");
+            var theChart = new Chart(ctx).Doughnut([
+                    {
+                        value: 300,
+                        color: "#F7464A",
+                        highlight: "#FF5A5E",
+                        label: "Red"
+                    },
+                    {
+                        value: 50,
+                        color: "#46BFBD",
+                        highlight: "#5AD3D1",
+                        label: "Green"
+                    },
+                    {
+                        value: 100,
+                        color: "#FDB45C",
+                        highlight: "#FFC870",
+                        label: "Yellow"
+                    }
+            ]);
 
         }
     };

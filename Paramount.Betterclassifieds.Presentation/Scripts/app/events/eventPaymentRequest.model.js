7a4d075763cd481c78f5cf53be702bde545b308b
Dejan Vasic <dejanvasic24@gmail.com>
2015-12-12 23:01:45 +1100

    Displaying an alert to user if their payment details are not configured

--- Paramount.Betterclassifieds.Presentation/Scripts/app/events/eventPaymentRequest.model.js
+++ Paramount.Betterclassifieds.Presentation/Scripts/app/events/eventPaymentRequest.model.js
@@ -5,6 +5,8 @@
         var me = this;
         me.selectedPaymentType = ko.observable(data.preferredPaymentType);
         me.eventId = ko.observable(data.eventId);
+        me.isPayPalConfigured = ko.observable(data.payPalEmail !== null);
+        me.isDirectDebitConfigured = ko.observable(data.directDebitDetails !== null);
 
         me.selectPayPal = function () {
             me.selectedPaymentType($paramount.PAYMENT.PAYPAL);
@@ -22,16 +24,26 @@
             return me.selectedPaymentType() === $paramount.PAYMENT.DIRECT_DEBIT;
         });
 
+        me.canSubmitPaymentRequest = ko.computed(function () {
+            if (me.selectedPaymentType() === $paramount.PAYMENT.PAYPAL && me.isPayPalConfigured()) {
+                return true;
+            }
+            if (me.selectedPaymentType() === $paramount.PAYMENT.DIRECT_DEBIT && me.isDirectDebitConfigured()) {
+                return true;
+            }
+            return false;
+        });
+
         me.submitPaymentRequest = function (element, event) {
             var $btn = $(event.target); // Grab the jQuery element from knockout
             $btn.button('loading');
 
             var paymentRequest = {
                 paymentMethod: me.selectedPaymentType(),
-                eventId : me.eventId()
+                eventId: me.eventId()
             }
             adDesignService.requestEventPayment(paymentRequest)
-                .error(function() {
+                .error(function () {
                     $btn.button('reset');
                 });
         }

e9e400bb486c6d4b8ed4309e2fb813593cadcb45
Dejan Vasic <dejanvasic24@gmail.com>
2016-01-29 23:32:27 +1100

    Cleaning up the unused code now for the location controller

--- Paramount.Betterclassifieds.Presentation/Controllers/LocationController.cs
+++ Paramount.Betterclassifieds.Presentation/Controllers/LocationController.cs
@@ -1,23 +1,16 @@
-﻿using System.IO;
-using System.Linq;
-using System.Net;
+﻿using System.Linq;
 using System.Web.Mvc;
-using Newtonsoft.Json;
-using Paramount.Betterclassifieds.Business.Location;
 using Paramount.Betterclassifieds.Business.Search;
-using Paramount.Betterclassifieds.Presentation.ViewModels.Location;
 
 namespace Paramount.Betterclassifieds.Presentation.Controllers
 {
     public class LocationController : Controller
     {
         private readonly ISearchService _searchService;
-        private readonly ILocationService _locationService;
 
-        public LocationController(ISearchService searchService, ILocationService locationService)
+        public LocationController(ISearchService searchService)
         {
             _searchService = searchService;
-            _locationService = locationService;
         }
 
         public ActionResult GetLocationOptions()
@@ -36,13 +29,6 @@ namespace Paramount.Betterclassifieds.Presentation.Controllers
                 .OrderBy(l => l.Text);
 
             return Json(list, JsonRequestBehavior.AllowGet);
-        }
-
-        public ActionResult GetTimezoneInfo(decimal latitude, decimal longitude)
-        {
-            // var request = WebRequest.Create("https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyBKrlWK9lGZBmkeHoPHAXHa1YjY_pd_z3I");
-            var zoneData = _locationService.GetTimezone(latitude, longitude);
-            return Json(zoneData, JsonRequestBehavior.AllowGet);
         }
     }
 }

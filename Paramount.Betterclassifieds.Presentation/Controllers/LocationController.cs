bef155ad301dd5504adec4cf63e025035a014bac
Dejan Vasic <dejanvasic24@gmail.com>
2016-01-29 22:45:19 +1100

    Making a google request for the timezone

--- Paramount.Betterclassifieds.Presentation/Controllers/LocationController.cs
+++ Paramount.Betterclassifieds.Presentation/Controllers/LocationController.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
+using System.Net;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using Paramount.Betterclassifieds.Business.Search;
+using Paramount.Betterclassifieds.Presentation.ViewModels.Location;
 
 namespace Paramount.Betterclassifieds.Presentation.Controllers
 {
@@ -29,6 +33,21 @@ namespace Paramount.Betterclassifieds.Presentation.Controllers
                 .OrderBy(l => l.Text);
 
             return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetTimezoneInfo(string latitude, string longitude)
+        {
+            var request = WebRequest.Create("https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyBKrlWK9lGZBmkeHoPHAXHa1YjY_pd_z3I");
+            var responseStream = request.GetResponse().GetResponseStream();
+            if (responseStream == null)
+                return new EmptyResult();
+
+            var reader = new StreamReader(responseStream);
+            var str = reader.ReadToEnd();
+            responseStream.Close();
+            reader.Close();
+            var googleResponseObj = JsonConvert.DeserializeObject<GoogleTimeZone>(str);
+            return Json(googleResponseObj, JsonRequestBehavior.AllowGet);
         }
     }
 }

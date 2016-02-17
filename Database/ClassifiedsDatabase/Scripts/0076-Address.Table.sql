2660b80ff71583991bb4cfbb2d0f442809d9ec91
Dejan Vasic <dejanvasic24@gmail.com>
2016-02-17 23:29:18 +1100

    Adding the sql data structure to hold the address information

---
+++ Database/ClassifiedsDatabase/Scripts/0076-Address.Table.sql
@@ -0,0 +1,30 @@
+﻿
+/****** Object:  Table [dbo].[Address]    Script Date: 17/02/2016 11:23:24 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Address](
+	[AddressId] [bigint] IDENTITY(1,1) NOT NULL,
+	[StreetNumber] [varchar](30) NULL,
+	[StreetName] [varchar](255) NULL,
+	[Suburb] [varchar](100) NULL,
+	[State] [varchar](50) NULL,
+	[Postcode] [varchar](5) NULL,
+	[Country] [varchar](100) NULL,
+ CONSTRAINT [PK_Address] PRIMARY KEY CLUSTERED 
+(
+	[AddressId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
---
+++ Database/ClassifiedsDatabase/Scripts/0077-Event.Column.AddressId.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE [Event]
+ADD AddressId BIGINT NULL
+
+GO
+
+ALTER TABLE [dbo].[Event]  WITH CHECK ADD  CONSTRAINT [FK_Event_Address] FOREIGN KEY([AddressId])
+REFERENCES [dbo].[Address] ([AddressId])
+GO

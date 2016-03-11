0b2dd3d20e2857add833412fab6cedcb6412f1c7
Dejan Vasic <dejanvasic24@gmail.com>
2016-03-11 23:55:21 +1100

    No more duplicating the routes and instead just heading straight to the RouteConfig

--- Paramount.Betterclassifieds.Tests/Controllers/ControllerTest.cs
+++ Paramount.Betterclassifieds.Tests/Controllers/ControllerTest.cs
@@ -54,37 +54,9 @@ namespace Paramount.Betterclassifieds.Tests.Controllers
             if (routes == null)
             {
                 routes = new RouteCollection();
-
-                // Ignored routes
-                routes.Ignore("{resource}.axd/{*pathInfo}");
-                routes.Ignore("{resource}.aspx/{*pathInfo}");
-                routes.Ignore("{resources}.ashx/{*pathInfo}");
-
-                // Images
-                routes.MapRoute("imageRoute", "img/{documentId}/{width}/{height}", new { controller = "Image", action = "Render", width = UrlParameter.Optional, height = UrlParameter.Optional });
-
-                // Ad route
-                routes.MapRoute("adRoute", "Ad/{title}/{id}", new { controller = "Listings", action = "ViewAd" });
-                routes.MapRoute("Event", "Event/{title}/{id}", new { controller = "Event", action = "ViewEventAd" }); // Need to be Pascal to suit the database record!
-
-                // routes.MapRoute("EventBooking", "Event/{title}/{id}/buy-ticket", new {controller = ""});
-
-                // Seo route
-                routes.MapRoute("seoName", "{seoName}/listings", new { controller = "Listings", action = "SeoAds" });
-
-                // Booking step routes
-                routes.MapRoute("bookingRoute1", "Booking/Step/1", new { controller = "Booking", action = "Step1" });
-                routes.MapRoute("bookingRoute2", "Booking/Step/2/{adType}", new { controller = "Booking", action = "Step2", adType = UrlParameter.Optional });
-                routes.MapRoute("bookingRoute3", "Booking/Step/3", new { controller = "Booking", action = "Step3" });
-
-                // Default
-                routes.MapRoute("defaultRoute", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
-
-
-                //routes.MapRoute("bookingRoute2", "Booking/Step/2/{adType}", new { controller = "Booking", action = "Step2", adType = UrlParameter.Optional });
-                //routes.MapRoute("default", "{controller}/{action}");
+                Paramount.Betterclassifieds.Presentation.RouteConfig.RegisterRoutes(routes);
             }
-
+            
             var mockRequestContext = new RequestContext(mockHttpContext.Object, routeData);
             var mockControllerContext = new ControllerContext(mockRequestContext, controller);
 

ea42b0a2f5e1ec7abc5a7e95b6f65c1beea8c98d
uchenjoku <github@uchenjoku.com>
2016-03-28 23:32:52 +1100

    add qr library

---
+++ Paramount.Betterclassifieds.Presentation/Framework/HtmlHelpers/HtmlHelperExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ZXing;
+using ZXing.Common;
+
+namespace Paramount.Betterclassifieds.Presentation.Framework.HtmlHelpers
+{
+    public static class HtmlHelperExtensions {
+        public static IHtmlString GenerateRelayQrCode(this HtmlHelper html, string qrValue, int height = 250, int width = 250, int margin = 0)
+        {
+            var barcodeWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions { Height = height, Width = width, Margin = margin }
+            };
+
+            using (var bitmap = barcodeWriter.Write(qrValue))
+            {
+                using (var stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, ImageFormat.Gif);
+
+                    var img = new TagBuilder("img");
+                    img.MergeAttribute("alt", "qr");
+                    img.Attributes.Add("src", string.Format("data:image/gif;base64,{0}",
+                        Convert.ToBase64String(stream.ToArray())));
+
+                    return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+                }
+            }
+        }
+
+
+    }
+}

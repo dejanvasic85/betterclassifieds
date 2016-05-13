b71773b71b00c1bece3141e04234ba50a38cba5b
Dejan Vasic <dejanvasic24@gmail.com>
2016-05-13 22:59:46 +1000

    The ticket selection is now working and js is moved to a file for minification

---
+++ Paramount.Betterclassifieds.Presentation/Scripts/app/events/invitation.ui.js
@@ -0,0 +1,29 @@
+﻿(function($, $paramount) {
+
+    'use strict';
+    $paramount.ui = $paramount.ui || {};
+    $paramount.ui.invitation = {
+        init : function(eventId, availableTickets) {
+            
+            var service = new $paramount.EventService();
+
+            // On ready
+            $(function() {
+                $('button[data-ticket]').on('click', function() {
+                    var $me = $(this);
+                    var selected = $me.data().ticket;
+                    var ticket = _.find(availableTickets, function(t) { return t.eventTicketId === selected });
+                    ticket.selectedQuantity = 1;
+
+                    var order = {
+                        eventId : eventId,
+                        tickets : [ticket]
+                    }
+
+                    service.startTicketOrder(order);
+                });
+            });
+        }
+    }
+    
+})(jQuery, $paramount)

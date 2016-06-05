87de515aef06a039e404301f1092c227d8232c5a
Dejan Vasic <dejanvasic24@gmail.com>
2016-06-05 22:40:52 +1000

    Updating the tickets email so it's a lot nicer with kandobay branding

---
+++ Database/BroadcastDatabase/Scripts-3.0/03-Update.EmailTemplate-EventTicketsBooked.sql
@@ -0,0 +1,36 @@
+﻿
+UPDATE	EmailTemplate
+SET		BodyTemplate = '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>KandoBay Notification</title>
+</head>
+<body style=''font-family: "Open Sans" ,"helvetica neue",helvetica,arial,sans-serif;font-weight:300;''>
+    <div style="padding-right: 15px;padding-left: 15px;margin-right: auto;margin-left: auto;border:1px solid #eee">
+        <div style="border-bottom:1px solid #e3e3e3; padding:15px">
+            <img alt="Logo" src="http://img.kandobay.com.au/kandobay_logo.png" height="100" />
+        </div>
+        <div class="row">
+            <h3>Congrats! You have tickets to "[/EventName/]"</h3>
+            <p>Your tickets should be attached to this email.</p>
+            <p>Full event details <a href="[/EventUrl/]">here.</a></p>
+        </div>
+    </div>
+    <div style="margin-top: 15px;background: #222;color: #f5f5f5;padding: 15px;font-size: 0.8em;">
+        <p>
+            <a target="_blank" href="http://kandobay.com.au" style="text-decoration: underline;color: #fff">Home</a> |
+            <a target="_blank" href="http://kandobay.com.au/Home/ContactUs" style="text-decoration: underline;color:#fff">Contact Us</a>
+        </p>
+        <p>
+            This is an automated email sent to you officially by
+            <a target="_blank" href="http://kandobay.com.au" style="color: #fff; text-decoration: underline">
+                kandobay.com.au
+            </a> for security reasons.
+            If you believe that you received this in error please ignore it or contact our friendly team.
+        </p>
+    </div>
+</body>
+</html>
+'
+WHERE DocType = 'EventTicketsBooked' AND Brand = 'KandoBay'

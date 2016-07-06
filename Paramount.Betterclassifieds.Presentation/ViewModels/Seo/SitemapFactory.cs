8654b661b530bd3abcb21e2fb6952a6cea541471
Dejan Vasic <dejanvasic24@gmail.com>
2016-07-07 07:29:38 +1000

    Neater code for the xml generation

--- Paramount.Betterclassifieds.Presentation/ViewModels/Seo/SitemapFactory.cs
+++ Paramount.Betterclassifieds.Presentation/ViewModels/Seo/SitemapFactory.cs
@@ -28,35 +28,13 @@ namespace Paramount.Betterclassifieds.Presentation.ViewModels.Seo
 
             foreach (var sitemapNode in GetNodes())
             {
-                XElement urlElement = new XElement(
-            xmlns + "sitemap",
-            new XElement(xmlns + "loc", Uri.EscapeUriString(sitemapNode.Url.ToLower())),
-            sitemapNode.LastModified == null ? null : new XElement(
-                xmlns + "lastmod",
-                sitemapNode.LastModified.Value.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:sszzz")),
-            sitemapNode.Frequency == null ? null : new XElement(
-                xmlns + "changefreq",
-                sitemapNode.Frequency.Value.ToString().ToLowerInvariant()),
-            sitemapNode.Priority == null ? null : new XElement(
-                xmlns + "priority",
-                sitemapNode.Priority.Value.ToString("F1", CultureInfo.InvariantCulture)));
+                XElement urlElement = new XElement(xmlns + "sitemap",
+                    new XElement(xmlns + "loc", Uri.EscapeUriString(sitemapNode.Url.ToLower())), 
+                        sitemapNode.LastModified == null ? null : new XElement(xmlns + "lastmod", sitemapNode.LastModified.Value.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:sszzz")), 
+                        sitemapNode.Frequency == null ? null : new XElement(xmlns + "changefreq", sitemapNode.Frequency.Value.ToString().ToLowerInvariant()), 
+                        sitemapNode.Priority == null ? null : new XElement(xmlns + "priority", sitemapNode.Priority.Value.ToString("F1", CultureInfo.InvariantCulture)));
+
                 root.Add(urlElement);
-
-                //var node = new XElement("sitemap");
-                //root.Add(node);
-                //var urlElement = new XElement(xmlns + "url", new XElement(xmlns + "loc", Uri.EscapeUriString(sitemapNode.Url)),
-
-                //    sitemapNode.LastModified == null
-                //        ? null
-                //        : new XElement(xmlns + "lastmod", sitemapNode.LastModified.Value.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:sszzz")),
-
-                //    sitemapNode.Frequency == null
-                //        ? null
-                //        : new XElement(xmlns + "changefreq", sitemapNode.Frequency.Value.ToString().ToLowerInvariant()),
-
-                //    sitemapNode.Priority == null
-                //        ? null
-                //        : new XElement(xmlns + "priority", sitemapNode.Priority.Value.ToString("F1", CultureInfo.InvariantCulture)));
             }
 
             XDocument document = new XDocument(root);

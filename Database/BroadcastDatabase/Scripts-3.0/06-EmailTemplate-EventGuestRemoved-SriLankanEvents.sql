46f0f38f7ea88cb04b00f00fd57b41cc2f1c8b30
Dejan Vasic <dejanvasic24@gmail.com>
2016-08-31 17:16:16 +1000

    Adding the email template for the event guest removed

---
+++ Database/BroadcastDatabase/Scripts-3.0/06-EmailTemplate-EventGuestRemoved-SriLankanEvents.sql
@@ -0,0 +1,41 @@
+﻿exec [EmailTemplate_Upsert]
+	@DocType = 'EventDailySummary',
+	@Brand = 'SriLankanEvents',
+	@SubjectTemplate = 'Your daily event summary for [/event-name/]',
+	@Description = 'Event daily summary',
+	@From = 'support@srilankanevents.com.au',
+	@BodyTemplate = '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sri Lankan Events Notification</title>
+</head>
+<body style=''font-family: "Open Sans" ,"helvetica neue",helvetica,arial,sans-serif;font-weight:300;''>
+    <div style="padding-right: 15px;padding-left: 15px;margin-right: auto;margin-left: auto;border:1px solid #eee">
+       <div class="row">
+            <h3>Ticket Canceled</h3>
+            <p>
+            	Hey there, we just wanted to let you know that your ticket number [/EventBookingTicketId/] for event <strong> [/EventName/] </strong> on [/EventDate/] has been canceled by the event organiser.
+            </p>
+            <p>
+            	Please visit the <a href="[/EventUrl/]">event page</a> to contact the organiser should 
+            	you have any issues with this.
+            </p>
+            <p>Thank you</p>
+        </div>
+    </div>
+    <div style="margin-top: 15px;background: #263745;color: #f5f5f5;padding: 15px;font-size: 0.8em;">
+        <p>
+            <a target="_blank" href="http://srilankanevents.com.au" style="text-decoration: underline;color: #fff">Home</a> |
+            <a target="_blank" href="http://srilankanevents.com.au/Home/ContactUs" style="text-decoration: underline;color:#fff">Contact Us</a>
+        </p>
+        <p>
+            This is an automated email sent to you officially by
+            <a target="_blank" href="http://srilankanevents.com.au" style="color: #fff; text-decoration: underline">
+                srilankanevents.com.au
+            </a> for security reasons.
+            If you believe that you received this in error please ignore it or contact our friendly team.
+        </p>
+    </div>
+</body>
+</html>';

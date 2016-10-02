ced6e8c7a93371edc2e7d1cb32fcd2c99c755ed6
Dejan Vasic <dejanvasic24@gmail.com>
2016-10-02 13:18:29 +1100

    Adding the dreaded hard - coded any location and area

---
+++ Database/ClassifiedsDatabase/Scripts-3.0/028-LocationAndArea.sql
@@ -0,0 +1,24 @@
+﻿DECLARE @anyLocationId AS INT,
+		@anyAreaId AS INT;
+
+SELECT @anyLocationId = LocationId
+FROM Location
+WHERE Title like '%Any Location%'
+
+IF @anyLocationId IS NULL
+begin
+	INSERT INTO Location (Title) VALUES (' Any Location');
+	SET @anyLocationId = @@IDENTITY;
+end
+
+
+
+SELECT @anyAreaId = LocationAreaId
+FROM LocationArea
+WHERE Title like '%Any Area%'
+
+IF @anyAreaId IS NULL
+begin
+	INSERT INTO LocationArea (LocationId, Title) VALUES (@anyLocationId, ' Any Area');
+	SET @anyLocationId = @@IDENTITY;
+end

bf62787b0927ce8ef88697f26802b16822ad00e7
Dejan Vasic <dejanvasic24@gmail.com>
2016-10-12 20:37:51 +1100

    Adding script that will setup an actual website in IIS

---
+++ setup-site.ps1
@@ -0,0 +1,49 @@
+﻿Import-Module WebAdministration
+
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value;
+    if($Invocation.PSScriptRoot)
+    {
+        $Invocation.PSScriptRoot;
+    }
+    Elseif($Invocation.MyCommand.Path)
+    {
+        Split-Path $Invocation.MyCommand.Path
+    }
+    else
+    {
+        $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf("\"));
+    }
+}
+
+
+$iisAppPoolName = "betterclassifieds"
+$iisAppPoolDotNetVersion = "v4.0"
+$iisAppName = "betterclassifieds"
+$currentDirectory = Get-ScriptDirectory
+$directoryPath = Join-Path -Path $currentDirectory -ChildPath "Paramount.Betterclassifieds.Presentation"
+
+#navigate to the app pools root
+cd IIS:\AppPools\
+
+#check if the app pool exists
+if (!(Test-Path $iisAppPoolName -pathType container))
+{
+    #create the app pool
+    $appPool = New-Item $iisAppPoolName
+    $appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value $iisAppPoolDotNetVersion
+}
+
+#navigate to the sites root
+cd IIS:\Sites\
+
+#check if the site exists
+if (Test-Path $iisAppName -pathType container)
+{
+    return
+}
+
+#create the site
+$iisApp = New-Item $iisAppName -bindings @{protocol="http";bindingInformation=":80:" + $iisAppName} -physicalPath $directoryPath
+$iisApp | Set-ItemProperty -Name "applicationPool" -Value $iisAppPoolName

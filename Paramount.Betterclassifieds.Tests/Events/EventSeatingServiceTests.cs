7ca654600bf9e4bce32a6421afc0352b5c66b95e
Dejan Vasic <dejanvasic24@gmail.com>
2017-06-05 18:40:40 +1000

    Starting the testing for the event seating service

---
+++ Paramount.Betterclassifieds.Tests/Events/EventSeatingServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using NUnit.Framework;
+using Paramount.Betterclassifieds.Business.Events;
+
+namespace Paramount.Betterclassifieds.Tests.Events
+{
+    [TestFixture]
+    public class EventSeatingServiceTests : TestContext<EventSeatingService>
+    {
+
+        private Mock<IEventRepository> _mockEventRepository;
+
+        [SetUp]
+        public void SetupDependencies()
+        {
+            _mockEventRepository = CreateMockOf<IEventRepository>();
+        }
+
+        [Test]
+        public void BookSeat_GetsSeats_UpdatesBookingId_SavesToRepository()
+        {
+            var eventId = 1;
+            var eventBookingTicketId = 200;
+            var seatNumber = "A1";
+            var seatingService = BuildTargetObject();
+            
+            seatingService.BookSeat(eventId, eventBookingTicketId, seatNumber);
+        }
+    }
+}

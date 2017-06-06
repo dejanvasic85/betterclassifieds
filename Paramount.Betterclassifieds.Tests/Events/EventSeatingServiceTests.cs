b60dd8e6e429ddb23e78d17d0917934c8b6f345e
Dejan Vasic <dejanvasic24@gmail.com>
2017-06-06 18:13:19 +1000

    More unit tests for the seating service

--- Paramount.Betterclassifieds.Tests/Events/EventSeatingServiceTests.cs
+++ Paramount.Betterclassifieds.Tests/Events/EventSeatingServiceTests.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
 using Paramount.Betterclassifieds.Business;
 using Paramount.Betterclassifieds.Business.Events;
 using Paramount.Betterclassifieds.Tests.Mocks;
+using System;
+using System.Linq;
 
 namespace Paramount.Betterclassifieds.Tests.Events
 {
@@ -13,7 +14,7 @@ namespace Paramount.Betterclassifieds.Tests.Events
 
         private Mock<IEventRepository> _mockEventRepository;
         private Mock<ILogService> _mockLogService;
-        
+
         [SetUp]
         public void SetupDependencies()
         {
@@ -33,7 +34,7 @@ namespace Paramount.Betterclassifieds.Tests.Events
                 .WithSeatNumber(seatNumber)
                 .WithEventBookingTicketId(null)
                 .Build();
-            
+
             // Setup service calls
             _mockLogService.SetupWithVerification(call => call.Info(It.IsAny<string>()));
 
@@ -69,9 +70,60 @@ namespace Paramount.Betterclassifieds.Tests.Events
                     It.Is<string>(s => s == seatNumber)),
                 result: null);
 
-            
+
             // Act
-            Assert.Throws<NullReferenceException>(() =>  seatingService.BookSeat(eventTicketId, eventBookingTicketId, seatNumber));
+            Assert.Throws<NullReferenceException>(() => seatingService.BookSeat(eventTicketId, eventBookingTicketId, seatNumber));
+        }
+
+        [Test]
+        public void GetSeatsForEvent_FiltersForRequestId_ReturnsSeatsWithReservationExpiry()
+        {
+            var eventId = 1;
+            var orderRequestId = "order-123";
+            var seatBuilder = new EventSeatBookingMockBuilder();
+            var seatReservationExpiry = DateTime.Now.AddMinutes(10);
+
+            var seats = new[]
+            {
+                seatBuilder.WithSeatNumber("Seat-1").Build(),
+                seatBuilder.WithSeatNumber("Seat-2").Build(),
+            };
+
+            var reservationBuilder = new EventTicketReservationMockBuilder();
+            var reservations = new[]
+            {
+                reservationBuilder
+                    .WithSeatNumber("Seat-1") // This should match and add the exiry
+                    .WithSessionId("123")
+                    .WithExpiryDateUtc(seatReservationExpiry)
+                    .Build(),
+
+                reservationBuilder
+                    .WithSeatNumber("Seat-2")
+                    .WithSessionId(orderRequestId) // This should be filtered out
+                    .WithExpiryDateUtc(seatReservationExpiry)
+                    .Build()
+            };
+
+            _mockEventRepository.SetupWithVerification(call => call.GetEventSeats(It.Is<int>(e => e == eventId)), result: seats);
+            _mockEventRepository.SetupWithVerification(call => call.GetCurrentReservationsForEvent(It.Is<int>(e => e == eventId)), result: reservations);
+
+            var service = BuildTargetObject();
+
+            var result = service.GetSeatsForEvent(eventId, orderRequestId).ToList();
+
+            // Assert
+
+            result.Count.IsEqualTo(2);
+
+            var availableSeat = result.FirstOrDefault(s => s.IsAvailable());
+            availableSeat.IsNotNull();
+            availableSeat.SeatNumber.IsEqualTo("Seat-2");
+
+            var reservedSeat = result.FirstOrDefault(s => !s.IsAvailable());
+            reservedSeat.IsNotNull();
+            reservedSeat.SeatNumber.IsEqualTo("Seat-1");
+            reservedSeat.ReservationExpiryUtc.IsNotNull();
         }
     }
 }

d60ca45e714e20c43716f03f680abdd7b380140e
Dejan Vasic <dejanvasic24@gmail.com>
2017-07-30 21:41:27 +1000

    Adding additional logging to the outbound services calls.

--- Paramount.Betterclassifieds.Presentation/Services/Mail/MailSender.cs
+++ Paramount.Betterclassifieds.Presentation/Services/Mail/MailSender.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using Paramount.Betterclassifieds.Business;
 using Paramount.Betterclassifieds.Presentation.Services.Mail;
 using RestSharp;
@@ -32,32 +33,41 @@ namespace Paramount.Betterclassifieds.Presentation.Services
 
         public void Send(string to, string body, string subject, params MailAttachment[] attachments)
         {
-            _logService.Info($"MailService: Preparing message for {to} subject: {subject}");
-
-            var request = new RestRequest();
-            request.AddParameter("domain", _applicationConfig.MailgunDomain, ParameterType.UrlSegment);
-            request.Resource = "{domain}/messages";
-            request.AddParameter("from", _clientConfig.EmailFromAddress);
-            request.AddParameter("to", to);
-            request.AddParameter("subject", subject);
-            request.AddParameter("html", body);
-            request.Method = Method.POST;
-
-            if (attachments != null)
+            try
             {
-                foreach (var attachment in attachments)
+                _logService.Info($"MailService: Preparing message for {to} subject: {subject}");
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                var request = new RestRequest();
+                request.AddParameter("domain", _applicationConfig.MailgunDomain, ParameterType.UrlSegment);
+                request.Resource = "{domain}/messages";
+                request.AddParameter("from", _clientConfig.EmailFromAddress);
+                request.AddParameter("to", to);
+                request.AddParameter("subject", subject);
+                request.AddParameter("html", body);
+                request.Method = Method.POST;
+
+                if (attachments != null)
                 {
-                    request.AddFileBytes("attachment",
-                        attachment.FileContents,
-                        attachment.Filename,
-                        attachment.ContentType);
+                    foreach (var attachment in attachments)
+                    {
+                        request.AddFileBytes("attachment",
+                            attachment.FileContents,
+                            attachment.Filename,
+                            attachment.ContentType);
+                    }
                 }
-            }
 
-            _restClient.ExecuteAsync(request, (r) =>
+                _restClient.ExecuteAsync(request, r =>
+                {
+                    _logService.Info("Mailgun completed successfully", sw.Elapsed);
+                });
+            }
+            catch (Exception ex)
             {
-                _logService.Info("Mailgun completed successfully");
-            });
+                _logService.Error("Failed to send email to " + to, ex);
+            }
         }
     }
 }
